698a60b0a18aee24d791275463cdc1a2711f4e6e
ozanbilgili <ozan.bilgili@hotmail.com>
2021-09-09 22:04:44 +0300

    First Commit

---
+++ Spam Detector with NLP/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Spam Detector with NLP
+//
+//  Created by Ozan Bilgili on 9.09.2021.
+//
+
+import UIKit
+import NaturalLanguage
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var spamLabel: UILabel?
+    @IBOutlet weak var sentimentLabel: UILabel?
+    @IBAction func checkButton(_ sender: UIButton) {
+        guard let message = self.messageTextView?.text else {
+            return
+        }
+        
+        detectSpam(message: message)
+        detectSentiment(message: message)
+    }
+
+
+    private func detectSpam(message: String) {
+        do {
+            let spamDetector = try NLModel(mlModel: SpamAnalyzer_1().model)
+            guard let prediction = spamDetector.predictedLabel(for: message) else {
+                print("Failed to predict result")
+                return
+            }
+            
+            spamLabel?.text = "spam status: \(prediction == "spam" ? "SPAM" : "NOT SPAM")"
+        } catch {
+            fatalError("Failed to load Natural Language Model: \(error)")
+        }
+    }
+    
+    // range -1.0 (negative) to 1.0 (positive)
+    private func detectSentiment(message: String) {
+        let tagger = NLTagger(tagSchemes: [.sentimentScore])
+        tagger.string = message
+        
+        let (sentiment, _) = tagger.tag(at: message.startIndex, unit: .paragraph, scheme: .sentimentScore)
+        
+        // it supports 7 languages: English, French, Italian, German, Spanish, Portuguese, and simplified Chinese.
+        guard let sentimentScore = sentiment?.rawValue else {
+            return
+        }
+        
+        sentimentLabel?.text = "sentiment score: \(sentimentScore)"
+    }
+}
+
